6292da9229bcf494ece0a38d40bc1a939d27b75f
SabasRocha <93341237+SabasRocha@users.noreply.github.com>
2022-11-14 17:19:49 -0500

    Inicio

---
+++ index.js
@@ -0,0 +1,28 @@
+ 
+const path = require('path');
+const express = require('express');
+
+const app = new express()
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'paginas/index.html'))
+
+});
+
+app.get('/actualizar', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'paginas/actualizar.html'))
+
+});
+
+app.get('/eliminar', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'paginas/eliminar.html'))
+
+});
+
+app.listen(4000 , () => {
+
+    console.log("Aplicacion corriendo en el puerto 4000", 4000)
+});
